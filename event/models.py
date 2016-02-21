da7412f6dcb205bda254e4d98ef6bc5fb5fcf4c7
narikin <yuki.iwnr@gmail.com>
2016-02-21 21:30:39 +0900

    Write event model

--- event/models.py
+++ event/models.py
@@ -1,5 +1,14 @@
+# coding: utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    """
+        Event model
+    """
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name.encode(u"utf-8")
